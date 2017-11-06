aa045f9cd8dafc35bc08caf0956cd6907835c9d8
Seong Lee <slee8@ebay.com>
2017-11-06 14:43:34 -0800

    fiedls grouping

--- src/main/java/wordcount/TopologyMain.java
+++ src/main/java/wordcount/TopologyMain.java
@@ -1,7 +1,9 @@
 package wordcount;
 
 import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
 import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.tuple.Fields;
 
 /**
  * Created by slee8 on 11/6/17.
@@ -11,11 +13,22 @@ public class TopologyMain {
         TopologyBuilder builder = new TopologyBuilder();
         builder.setSpout("word-reader", new WordReader());
         builder.setBolt("word-counter", new WordCounter(), 2)
-                .shuffleGrouping("word-reader");
+                .fieldsGrouping("word-reader", new Fields("word"));
+//                .shuffleGrouping("word-reader");
 
         Config conf = new Config();
         conf.put("fileToRead", "/Users/slee8/sample.txt");
         conf.put("dirToWrite", "/Users/slee8/wordCountoutput/");
         conf.setDebug(true);
+
+        LocalCluster cluster = new LocalCluster();
+        try {
+            cluster.submitTopology("WordCoutner-Topology", conf, builder.createTopology());
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            cluster.shutdown();
+        }
     }
 }
